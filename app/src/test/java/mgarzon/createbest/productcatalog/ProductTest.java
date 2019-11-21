d868c5dba89576b12d98815c05ed0395696db4ca
Saffat Aziz <saziz028@uottawa.ca>
2019-11-21 11:06:33 -0500

    fixes

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -19,7 +19,7 @@ public class ProductTest {
     @Test
     public void checkProductPrice() {
         // I am making this test to fail
-        Product aProduct = new Product("1", "DELL MONITOR", 0);
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals(180.0, aProduct.getPrice(),0);
     }
 
